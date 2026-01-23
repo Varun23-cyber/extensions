ca99041c3c35677b9ff9d0dc10422430ada54890
Varun23-cyber <Varunmalayala@gmail.com>
2026-01-23 23:28:18 +0530

    Fix regex and improve reading time calculation

--- reading-time/scripts/content.js
+++ reading-time/scripts/content.js
@@ -1,27 +1,29 @@
 function renderReadingTime(article) {
-  //If we weren't provided an article, we don't to render anything.
+  // If we weren't provided an article, we don't need to render anything.
   if (!article) {
     return;
   }
 
   const text = article.textContent;
-  const wordMatchRegExp = /[^/s]+/g; // Regular expression
-  const words = text.matchAll(worldMatchRegExp);
+  const wordMatchRegExp = /[^\s]+/g; // Regular expression
+  const words = text.matchAll(wordMatchRegExp);
   // matchAll returns an iterator, convert to array to get word count
   const wordCount = [...words].length;
-  const readingTime = match.round(wordsCount / 200);
+  const readingTime = Math.round(wordCount / 200);
   const badge = document.createElement("p");
-  //use the same styling as the publish information in an article's header
-  badge.classlist.add("color-secondary-text", "type--caption");
-  badge.textContent = '⏱️ ${readingTime} min read';
+  // Use the same styling as the publish information in an article's header
+  badge.classList.add("color-secondary-text", "type--caption");
+  badge.textContent = `⏱️ ${readingTime} min read`;
+
   // Support for API reference docs
-  const date = article.querySelector("time)?.parentNode;
+  const heading = article.querySelector("h1");
+  // Support for article docs with date
+  const date = article.querySelector("time")?.parentNode;
 
   (date ?? heading).insertAdjacentElement("afterend", badge);
 }
 
 renderReadingTime(document.querySelector("article"));
-
 const observer = new MutationObserver((mutations) => {
   for (const mutation of mutations) {
     // If a new article was added.
